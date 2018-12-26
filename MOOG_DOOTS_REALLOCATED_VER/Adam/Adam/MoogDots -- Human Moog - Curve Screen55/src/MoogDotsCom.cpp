db06b82ea851e4961015927b9b35d0edda94a292
avielkar <avraham.elkar@gmail.com>
2018-12-26 15:35:47 +0200

    Adding logs for the chances to be at the correct position.

--- MOOG_DOOTS_REALLOCATED_VER/Adam/Adam/MoogDots -- Human Moog - Curve Screen55/src/MoogDotsCom.cpp
+++ MOOG_DOOTS_REALLOCATED_VER/Adam/Adam/MoogDots -- Human Moog - Curve Screen55/src/MoogDotsCom.cpp
@@ -2593,6 +2593,25 @@ bool MoogDotsCom::CheckMoogAtFinal(double maxDifferentialError)
 	finalForwardPosition->surge = m_finalForwardMovementPosition.surge;
 	finalForwardPosition->yaw = m_finalForwardMovementPosition.yaw;
 
+	//first chance to be at the correct position.
+	if (CheckMoogAtCorrectPosition(finalForwardPosition, maxDifferentialError))
+	{
+		return true;
+	}
+	//wait for the next chance.
+	Sleep(CORRECT_POSITION_CHACNE_TIME);
+
+	//second chance to be at the correct position.
+	WRITE_LOG(m_logger->m_logger, "Second chance - Checking robot is at final position.");
+	if (CheckMoogAtCorrectPosition(finalForwardPosition, maxDifferentialError))
+	{
+		return true;
+	}
+	//wait for the next chance.
+	Sleep(CORRECT_POSITION_CHACNE_TIME);
+
+	//third and last chance to be at the correct position.
+	WRITE_LOG(m_logger->m_logger, "Third chance - Checking robot is at final position.");
 	return CheckMoogAtCorrectPosition(finalForwardPosition, maxDifferentialError);
 }
 
